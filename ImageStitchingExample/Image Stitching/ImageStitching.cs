4769a36e34a6593e5fa6064f0729227b69c24943
Aaron Paul Jones <jonesaaron993@gmail.com>
2021-12-07 23:02:11 -0600

    Converted project from Framwork to Core

---
+++ ImageStitchingExample/Image Stitching/ImageStitching.cs
@@ -0,0 +1,83 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.Stitching;
+using Emgu.CV.Util;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Image_Stitching_Emgucv
+{
+    /// <summary>
+    /// Contains all methods to stitch images together
+    /// </summary>
+    class ImageStitching
+    {
+        /// <summary>
+        /// Pass a path to a folder of images and read in all those images and store them in a list
+        /// </summary>
+        /// <param name="path">The path to where the folder where the images are kept</param>
+        /// <returns>A list of Mat objects</returns>
+        public static List<Mat> GetImages(string path)
+        {
+            //Store all paths to each individual file in the directory in an array
+            string[] files = Directory.GetFiles(path);
+
+            //Declare a list of Mat to store all images loaded in
+            List<Mat> outputList = new List<Mat>();
+
+            //For each item in the files array, read in the image and store it in the list
+            foreach(var image in files)
+            {
+                //Read in the image and store it as a mat object
+                Mat img = CvInvoke.Imread(image, ImreadModes.AnyColor);
+
+                //Add the mat object to the list
+                outputList.Add(img);
+            }
+
+            //Return the list of read in mat objects
+            return outputList;
+        }
+
+        /// <summary>
+        /// Stitch images together
+        /// </summary>
+        /// <param name="images">The list of images to stitch</param>
+        /// <returns>A final stitched image</returns>
+        public static Mat StichImages(List<Mat> images)
+        {
+            //Declare the Mat object that will store the final output
+            Mat output = new Mat();
+
+            //Declare a vector to store all images from the list
+            VectorOfMat matVector = new VectorOfMat();
+
+            //Push all images in the list into a vector
+            foreach (Mat img in images)
+            {
+                matVector.Push(img);
+            }
+
+            //Declare a new stitcher
+            Stitcher stitcher = new Stitcher();
+
+            //Declare the type of detector that will be used to detect keypoints
+            Brisk detector = new Brisk();
+
+            //Here are some other detectors that you can try
+            //ORBDetector detector = new ORBDetector();
+            //KAZE detector = new KAZE();
+            //AKAZE detector = new AKAZE();
+
+            //Set the stitcher class to use the specified detector declared above
+            stitcher.SetFeaturesFinder(detector);
+
+            //Stitch the images together
+            stitcher.Stitch(matVector, output);
+
+            //Return the final stiched image
+            return output;
+        }
+    }
+}
---
+++ ImageStitchingExample/Image Stitching/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Emgu.CV;
+using Image_Stitching_Emgucv;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Image_Stitching
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStitch_Click(object sender, RoutedEventArgs e)
+        {
+            //Change this file path to the path where the images you want to stich are located
+            string filePath = @"./Images/";
+
+            //Read all the images from the path and store them in a list
+            List<Mat> images = ImageStitching.GetImages(filePath);
+
+            //Stitch the images that are located in the list
+            Mat output = ImageStitching.StichImages(images);
+
+            //Cath the error if the stitch image is null
+            try
+            {
+                //Convert the Mat object to a bitmap
+                Bitmap img = output.ToBitmap();
+
+                //Using the method below, convert the bitmap to an imagesource
+                imgOutput.Source = ImageSourceFromBitmap(img);
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("The images in the folder either couldn't be stitched, or the folder is empty");
+            }
+        }
+
+        //Convert a bitmap to imagesource
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+    }
+}
